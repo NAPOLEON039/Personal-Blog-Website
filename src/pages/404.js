2320d2de61d881ce36217920c3061c1a444309b1
Nihar Raote <nihar.raote@gmail.com>
2020-05-09 20:30:19 +0530

    Modified the 404 page a little

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,6 +8,7 @@ const NotFoundPage = () => (
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>Go to localhost:8000 or check your internet connection</p>
   </Layout>
 )
 
